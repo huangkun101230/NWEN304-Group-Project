63cc0c9a7cc167a54e7b0ee69ab421b9f0b24340
Rowan Self <rowan.self@gmail.com>
2017-05-22 14:14:32 +1200

    added not null for proper cols

--- database-init.sql
+++ database-init.sql
@@ -1,3 +1,3 @@
-CREATE TABLE users (user_id SERIAL PRIMARY KEY, username TEXT, password TEXT, cart TEXT ARRAY );
-CREATE TABLE user_cart (cart_id SERIAL PRIMARY KEY,user_id INTEGER,items TEXT ARRAY);
-CREATE TABLE products (products_id cart_id SERIAL PRIMARY KEY, product_name TEXT, product_des TEXT,price INTEGER,picture_dir TEXT);
+CREATE TABLE users (user_id NOT NULL SERIAL PRIMARY KEY, username NOT NULL TEXT, password NOT NULL TEXT, cart TEXT ARRAY );
+CREATE TABLE user_cart (id NOT NULL PRIMARY KEY,items TEXT ARRAY);
+CREATE TABLE products (products_id NOT NULL SERIAL PRIMARY KEY, product_name NOT NULL TEXT, product_des NOT NULL TEXT,price INTEGER,picture_dir TEXT);
