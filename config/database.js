672a8968e2b7ca419f4745ea78e4ad6e4d78b9e2
Rowan Self <rowan.self@gmail.com>
2017-05-29 10:07:26 +1200

    added support for the nodejs app in the docker compose file

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 var connectionString = process.env.DATABASE_URL || "postgres://root@localhost:5432/shopping";
 var pg = require('pg');
 var client = new pg.Client(connectionString);
+
+
 client.connect(function(err, client) {
     if (err) {
         throw err;
