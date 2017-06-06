acc604ef3bb2f2c7d8f898bebaaa7064509a3e7c
Rowan Self <rowan.self@gmail.com>
2017-06-06 21:10:50 +1200

    added logic to create fake data for products db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var port = process.env.PORT || 8080;
 var faker = require('faker');
 var session = require('express-session');
 var appRoot = require('app-root-path');
+var models = require('/models');
 require(appRoot+"/database/db-init.js");
 app.use(express.static(appRoot+'/public'));
 //app.use(express.urlencoded());
@@ -21,8 +22,25 @@ app.use(session({
 
 app.use(require('./routes/index'));
 
+models.sequelize.sync().then(function () {
+  var products =  models.products;
+  var count;
+  products.count().then(function(c){
+    count = c;
+  });
+  if (count == 0) {
+    products
+      .build({
+        product_name: faker.commerce.productName,
+        product_des: faker.lorem.sentences,
+        price: faker.commerce.price,
+        in_stock: faker.random.number
+      })
+      .save();
+  }
 
-app.listen(port, function () {
-	console.log('Example app listening on port 8080!');
+  app.listen(port, function () {
+    console.log('Example app listening on port 8080!');
+  });
 });
 module.exports = app;
