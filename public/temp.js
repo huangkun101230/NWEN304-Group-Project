d1785f32d720c3b0115f058c4ede69f22fd7083a
HeycomRoy <cui.yao.yu@gmail.com>
2017-06-20 19:12:23 +1200

    UPDATES: further updates on the cart and search

--- public/temp.js
+++ public/temp.js
@@ -2,6 +2,8 @@ $(document).ready(function(e) {
 
     var url_add = 'http://localhost:8080';
     var isLoggedin = false;
+    var username = '';
+    var password = '';
 // the nav "shop" is clicked
     $('#shop').click(function () {
         console.log("display products");
@@ -112,8 +114,8 @@ $(document).ready(function(e) {
         width: 350,
         buttons: {
             "Log-in": function () {
-                var username = $('#login_user_name').val();
-                var password = $('#login_password').val();
+                username = $('#login_user_name').val();
+                password = $('#login_password').val();
                 $('response_msg').empty();
                 if($.trim(username).length === 0 || $.trim(password).length === 0){
                     $('.response_msg').append('<p>User name/pass word can not be empty!</p> ');
@@ -128,6 +130,7 @@ $(document).ready(function(e) {
                         data: JSON.stringify({user: username, pass: password}),
                         success: function (user) {
                             console.log("successfully logged in");
+                            //window.alert(user.user);
                             // if (user.user === username && user.pass === password) {//TODO this condition is not working
                             //     window.alert("go");
                                 // $('#nav #login h2').text('Log out');
@@ -163,8 +166,87 @@ $(document).ready(function(e) {
         isLoggedin = false;
         $('#login').css("display","normal");
         $('#logout').css("display","none");
+        $.ajax({
+            url: url_add+'/logout',
+            type:'GET',
+            dataType: 'json',
+            success: function (response) {
 
+                window.alert(response);
+
+            },
+            error: function (error) {
+                console.log("");
+            }
+        });
     });
 
+//To add to the cart when the button is clicked
+    $('#display_content').on('click', '.add_cart', function () {
+        $.ajax({
+            url: url_add+'/addToCart',
+            type: 'POST',
+            dataType: 'json',
+            contentType: "application/json",
+            data: JSON.stringify( {user: user, pass:password}),
+            success: function (response) {
+
+            },
+            error: function(error){
+
+            }
+        });
+    });
+
+//Shopping Cart
+    $('#cart').click(function () {
+        $('#display_content').empty();
+        $display_content = $('#display_content');
+        $.ajax({
+            url: url_add+'/cart',//may change this later for the according the serverside
+            type:'GET',
+            dataType: 'json',
+            success: function (products) {
+                $.each(products, function (i, product) {
+                    $display_content.append(
+                        '<li class="products_list">'+
+                            '<img src="'+product.picture_dir+'" alt="'+product.product_name+'">'+
+                            '<span class="products_des">'+
+                                '<h2>'+product.product_name+'</h2>'+
+                                '<p>'+product.product_des+'</p>'+
+                                '<p>&#36; '+product.price+'</p>'+
+                            '</span>'+
+                            '<span class="add_cart"></span>' +
+                        '</li>'
+                    );
+                });
+            },
+            error: function (error) {
+                console.log("Unsuccessful to load products from database");
+            }
+        });
+    });
+
+//Search the product
+    $('#search a').click(function () {
+        var search_content=$('#search input').val();
+        if($search_content !== '') {
+            // window.alert($search_content);
+            $.ajax({
+                url: url_add + '/search',
+                type: 'POST',
+                dataType: 'json',
+                contentType: "application/json",
+                data: JSON.stringify({product_name: search_content}),//TODO search for the product name
+                success: function (product) {
+                //found or not found
+                },
+                error: function (error) {
+
+                }
+            });
+        }
+        $('#search input').val('');
+    });
 
 });//ends
